1da7f7ccf9dd90333c1b8547dad6f89c616b631b
xinxin13 <xinxin13@huawei.com>
2022-11-23 17:13:06 +0800

    fix return
    Signed-off-by: xinxin13 <xinxin13@huawei.com>

--- interfaces/kits/js/bundlemgr/bundle_mgr.cpp
+++ interfaces/kits/js/bundlemgr/bundle_mgr.cpp
@@ -9132,7 +9132,7 @@ NativeValue* JsBundleMgr::OnQueryAbilityInfos(NativeEngine &engine, NativeCallba
             std::string queryAbilityInfosErrData;
             if (info->getCache) {
                 APP_LOGD("has cache,no need to query from host");
-                task.ResolveWithErr(engine, info->cacheAbilityInfos);
+                task.RejectWithCustomize(engine, CreateJsValue(engine, 0), info->cacheAbilityInfos);
                 return;
             }
             if (errCode != ERR_OK) {
