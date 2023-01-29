62a644c73300b4929ca1f39610d8be3b40ab0626
耿文广 <gengwenguang@huawei.com>
2023-01-29 08:10:50 +0000

    update services/bundlemgr/test/unittest/bms_bundle_installer_test/bms_bundle_installer_test.cpp.
    Signed-off-by: 耿文广 <gengwenguang@huawei.com>

--- services/bundlemgr/test/unittest/bms_bundle_installer_test/bms_bundle_installer_test.cpp
+++ services/bundlemgr/test/unittest/bms_bundle_installer_test/bms_bundle_installer_test.cpp
@@ -2410,7 +2410,7 @@ HWTEST_F(BmsBundleInstallerTest, InstallChecker_0100, Function | SmallTest | Lev
  * @tc.desc: 1. BundleInstallChecker
 */
 HWTEST_F(BmsBundleInstallerTest, InstallChecker_0200, Function | SmallTest | Level0)
-{    
+{
     std::string bundlePath = RESOURCE_ROOT_PATH + BUNDLE_BACKUP_TEST + "rpcid.sc";
     std::vector<std::string> bundlePaths;
     bundlePaths.push_back(bundlePath);
