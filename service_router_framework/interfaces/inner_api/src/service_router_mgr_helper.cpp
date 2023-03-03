0990f853da910badbfb2e3fe3f5f7ce274c49254
xucheng <xucheng2@huawei.com>
2023-03-03 09:28:11 +0000

    code reviwe fix
    Signed-off-by: xucheng <xucheng2@huawei.com>
    Change-Id: I52217d5e8a5702f7a53ab56110838fb61b78cd41

--- service_router_framework/interfaces/inner_api/src/service_router_mgr_helper.cpp
+++ service_router_framework/interfaces/inner_api/src/service_router_mgr_helper.cpp
@@ -33,7 +33,7 @@ ServiceRouterMgrHelper::ServiceRouterMgrHelper()
 ServiceRouterMgrHelper::~ServiceRouterMgrHelper()
 {}
 
-void ServiceRouterMgrHelper:: OnRemoteDiedHandle()
+void ServiceRouterMgrHelper::OnRemoteDiedHandle()
 {
     APP_LOGE("Remove service died.");
     SetServiceRouterMgr(nullptr);
@@ -118,7 +118,7 @@ sptr<IServiceRouterManager> ServiceRouterMgrHelper::GetServiceRouterMgr()
     {
         std::unique_lock<std::mutex> lock(cvLock_);
         auto waitState = mgrConn_.wait_for(lock, std::chrono::milliseconds(LOAD_SA_TIMEOUT_MS),
-            [this] (){ return isReady; });
+            [this](){ return isReady; });
         if (!waitState) {
             return nullptr;
         }
