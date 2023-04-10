5708a1d10e2bbb61a9bdac279e2596ff64df7da5
wangtiantian <wangtiantian19@huawei.com>
2023-04-10 10:51:00 +0800

    IssueNo:#I6TZV7 Description:adapt userId Sig:SIG_ApplicaitonFramework Feature or Bugfix:Feature Binary Source:No Signed-off-by: wangtiantian <wangtiantian19@huawei.com>

--- services/bundlemgr/src/bundle_data_mgr.cpp
+++ services/bundlemgr/src/bundle_data_mgr.cpp
@@ -4667,6 +4667,19 @@ bool BundleDataMgr::IsPreInstallApp(const std::string &bundleName)
 ErrCode BundleDataMgr::GetSpecifiedDistributionType(
     const std::string &bundleName, std::string &specifiedDistributionType)
 {
+    APP_LOGD("GetSpecifiedDistributionType bundleName: %{public}s", bundleName.c_str());
+    std::lock_guard<std::mutex> lock(bundleInfoMutex_);
+    auto infoItem = bundleInfos_.find(bundleName);
+    if (infoItem == bundleInfos_.end()) {
+        APP_LOGE("bundleName: %{public}s does not exist", bundleName.c_str());
+        return ERR_BUNDLE_MANAGER_BUNDLE_NOT_EXIST;
+    }
+    int32_t userId = AccountHelper::GetCurrentActiveUserId();
+    int32_t responseUserId = infoItem->second.GetResponseUserId(userId);
+    if (responseUserId == Constants::INVALID_USERID) {
+        APP_LOGE("bundleName: %{public}s does not exist in current userId", bundleName.c_str());
+        return ERR_BUNDLE_MANAGER_BUNDLE_NOT_EXIST;
+    }
     if (!DelayedSingleton<AppProvisionInfoManager>::GetInstance()->GetSpecifiedDistributionType(bundleName,
         specifiedDistributionType)) {
         APP_LOGE("bundleName:%{public}s GetSpecifiedDistributionType failed.", bundleName.c_str());
@@ -4678,6 +4691,19 @@ ErrCode BundleDataMgr::GetSpecifiedDistributionType(
 ErrCode BundleDataMgr::GetAdditionalInfo(
     const std::string &bundleName, std::string &additionalInfo)
 {
+    APP_LOGD("GetAdditionalInfo bundleName: %{public}s", bundleName.c_str());
+    std::lock_guard<std::mutex> lock(bundleInfoMutex_);
+    auto infoItem = bundleInfos_.find(bundleName);
+    if (infoItem == bundleInfos_.end()) {
+        APP_LOGE("bundleName: %{public}s does not exist", bundleName.c_str());
+        return ERR_BUNDLE_MANAGER_BUNDLE_NOT_EXIST;
+    }
+    int32_t userId = AccountHelper::GetCurrentActiveUserId();
+    int32_t responseUserId = infoItem->second.GetResponseUserId(userId);
+    if (responseUserId == Constants::INVALID_USERID) {
+        APP_LOGE("bundleName: %{public}s does not exist in current userId", bundleName.c_str());
+        return ERR_BUNDLE_MANAGER_BUNDLE_NOT_EXIST;
+    }
     if (!DelayedSingleton<AppProvisionInfoManager>::GetInstance()->GetAdditionalInfo(bundleName,
         additionalInfo)) {
         APP_LOGE("bundleName:%{public}s GetAdditionalInfo failed.", bundleName.c_str());
