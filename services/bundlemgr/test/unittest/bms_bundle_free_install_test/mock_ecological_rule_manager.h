d88d2ddc44ff1646597ce32f18d121f6e906da32
c00576431 <caozilong2@huawei.com>
2023-05-29 22:03:33 +0800

    免安装测试用例补充
    Signed-off-by: c00576431 <caozilong2@huawei.com>

--- services/bundlemgr/test/unittest/bms_bundle_free_install_test/mock_ecological_rule_manager.h
+++ services/bundlemgr/test/unittest/bms_bundle_free_install_test/mock_ecological_rule_manager.h
@@ -116,17 +116,6 @@ class EcologicalRuleMgrService : public EcologicalRuleMgrStub {
 public:
     EcologicalRuleMgrService() {};
     virtual ~EcologicalRuleMgrService() override = default;
-
-    MOCK_METHOD3(QueryFreeInstallExperience, int32_t(const Want &want,
-        const ErmsParams::CallerInfo &callerInfo, ErmsParams::ExperienceRule &rule));
-    MOCK_METHOD5(EvaluateResolveInfos, int32_t(const Want &want, const ErmsParams::CallerInfo &callerInfo,
-        int32_t type, std::vector<AbilityInfo> &abilityInfos, std::vector<ExtensionAbilityInfo> extensionInfos));
-    MOCK_METHOD3(QueryStartExperience, int32_t(const Want &want,
-        const ErmsParams::CallerInfo &callerInfo, ErmsParams::ExperienceRule &rule));
-    MOCK_METHOD2(QueryPublishFormExperience, int32_t(const Want &want, ErmsParams::ExperienceRule &rule));
-    MOCK_METHOD3(IsSupportPublishForm, int32_t(const Want &want,
-        const ErmsParams::CallerInfo &callerInfo, ErmsParams::ExperienceRule &rule));
-    MOCK_METHOD0(QueryLastSyncTime, long());
 };
 
 class MockEcologicalRuleMgrService : public EcologicalRuleMgrService {
