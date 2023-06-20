99b65862d8077534e18fea86374d5b2e534b31e1
zhangxinyu <zhangxinyu74@huawei.com>
2023-06-20 13:47:39 +0000

    系统服务接口管控
    Signed-off-by: zhangxinyu <zhangxinyu74@huawei.com>
    Change-Id: I7eeaa74e695108ee4d9678b25b03989604459b69

--- interfaces/inner_api/appexecfwk_core/include/bundle_framework_core_ipc_interface_code.h
+++ interfaces/inner_api/appexecfwk_core/include/bundle_framework_core_ipc_interface_code.h
@@ -18,99 +18,10 @@
 
 #include <stdint.h>
 
-/* SAID: 401 */
 namespace OHOS {
 namespace AppExecFwk {
-enum class AppControlManagerInterfaceCode : uint32_t {
-    ADD_APP_INSTALL_CONTROL_RULE = 0,
-    DELETE_APP_INSTALL_CONTROL_RULE,
-    CLEAN_APP_INSTALL_CONTROL_RULE,
-    GET_APP_INSTALL_CONTROL_RULE,
-    ADD_APP_RUNNING_CONTROL_RULE,
-    DELETE_APP_RUNNING_CONTROL_RULE,
-    CLEAN_APP_RUNNING_CONTROL_RULE,
-    GET_APP_RUNNING_CONTROL_RULE,
-    GET_APP_RUNNING_CONTROL_RULE_RESULT,
-    SET_DISPOSED_STATUS,
-    DELETE_DISPOSED_STATUS,
-    GET_DISPOSED_STATUS,
-    CONFIRM_APP_JUMP_CONTROL_RULE,
-    ADD_APP_JUMP_CONTROL_RULE,
-    DELETE_APP_JUMP_CONTROL_RULE,
-    DELETE_APP_JUMP_CONTROL_RULE_BY_CALLER,
-    DELETE_APP_JUMP_CONTROL_RULE_BY_TARGET,
-    GET_APP_JUMP_CONTROL_RULE,
-};
 
-enum class BundleEventCallbackInterfaceCode : uint32_t {
-    ON_RECEIVE_EVENT,
-};
-
-enum class BundleInstallerInterfaceCode : uint32_t {
-    INSTALL = 0,
-    INSTALL_MULTIPLE_HAPS,
-    UNINSTALL,
-    UNINSTALL_MODULE,
-    UNINSTALL_BY_UNINSTALL_PARAM,
-    RECOVER,
-    INSTALL_SANDBOX_APP,
-    UNINSTALL_SANDBOX_APP,
-    CREATE_STREAM_INSTALLER,
-    DESTORY_STREAM_INSTALLER,
-};
-
-enum class BundleStatusCallbackInterfaceCode : uint32_t {
-    ON_BUNDLE_STATE_CHANGED,
-};
-
-enum class BundleStreamInstallerInterfaceCode : uint32_t {
-    CREATE_STREAM = 0,
-    STREAM_INSTALL = 1,
-    CREATE_SHARED_BUNDLE_STREAM = 2,
-    CREATE_SIGNATURE_FILE_STREAM = 3
-};
-
-enum class CleanCacheCallbackInterfaceCode : uint32_t {
-    ON_CLEAN_CACHE_CALLBACK,
-};
-
-enum class StatusReceiverInterfaceCode : uint32_t {
-    ON_STATUS_NOTIFY,
-    ON_FINISHED,
-};
-
-enum class DefaultAppInterfaceCode : uint32_t {
-    IS_DEFAULT_APPLICATION = 0,
-    GET_DEFAULT_APPLICATION = 1,
-    SET_DEFAULT_APPLICATION = 2,
-    RESET_DEFAULT_APPLICATION = 3,
-};
-
-enum class OverlayManagerInterfaceCode : uint32_t {
-    GET_ALL_OVERLAY_MODULE_INFO = 0,
-    GET_OVERLAY_MODULE_INFO_BY_NAME = 1,
-    GET_OVERLAY_MODULE_INFO = 2,
-    GET_TARGET_OVERLAY_MODULE_INFOS = 3,
-    GET_OVERLAY_MODULE_INFO_BY_BUNDLE_NAME = 4,
-    GET_OVERLAY_BUNDLE_INFO_FOR_TARGET = 5,
-    GET_OVERLAY_MODULE_INFO_FOR_TARGET = 6,
-    SET_OVERLAY_ENABLED = 7,
-    SET_OVERLAY_ENABLED_FOR_SELF = 8,
-};
-
-enum class QuickFixManagerInterfaceCode : uint32_t {
-    DEPLOY_QUICK_FIX = 0,
-    SWITCH_QUICK_FIX = 1,
-    DELETE_QUICK_FIX = 2,
-    CREATE_FD = 3
-};
-
-enum class QuickFixStatusCallbackInterfaceCode : uint32_t {
-    ON_PATCH_DEPLOYED = 1,
-    ON_PATCH_SWITCHED = 2,
-    ON_PATCH_DELETED = 3
-};
-
+/* SAID: 401 */
 enum class BundleMgrInterfaceCode : uint32_t {
     GET_APPLICATION_INFO = 0,
     GET_APPLICATION_INFOS,
@@ -233,6 +144,108 @@ enum class BundleMgrInterfaceCode : uint32_t {
     DEL_EXT_NAME_OR_MIME_TO_APP,
 };
 
+/* SAID: 401-85 */
+enum class AppControlManagerInterfaceCode : uint32_t {
+    ADD_APP_INSTALL_CONTROL_RULE = 0,
+    DELETE_APP_INSTALL_CONTROL_RULE,
+    CLEAN_APP_INSTALL_CONTROL_RULE,
+    GET_APP_INSTALL_CONTROL_RULE,
+    ADD_APP_RUNNING_CONTROL_RULE,
+    DELETE_APP_RUNNING_CONTROL_RULE,
+    CLEAN_APP_RUNNING_CONTROL_RULE,
+    GET_APP_RUNNING_CONTROL_RULE,
+    GET_APP_RUNNING_CONTROL_RULE_RESULT,
+    SET_DISPOSED_STATUS,
+    DELETE_DISPOSED_STATUS,
+    GET_DISPOSED_STATUS,
+    CONFIRM_APP_JUMP_CONTROL_RULE,
+    ADD_APP_JUMP_CONTROL_RULE,
+    DELETE_APP_JUMP_CONTROL_RULE,
+    DELETE_APP_JUMP_CONTROL_RULE_BY_CALLER,
+    DELETE_APP_JUMP_CONTROL_RULE_BY_TARGET,
+    GET_APP_JUMP_CONTROL_RULE,
+};
+
+/* SAID: 401-96 */
+enum class BundleEventCallbackInterfaceCode : uint32_t {
+    ON_RECEIVE_EVENT,
+};
+
+/* SAID: 401-43 */
+enum class BundleInstallerInterfaceCode : uint32_t {
+    INSTALL = 0,
+    INSTALL_MULTIPLE_HAPS,
+    UNINSTALL,
+    UNINSTALL_MODULE,
+    UNINSTALL_BY_UNINSTALL_PARAM,
+    RECOVER,
+    INSTALL_SANDBOX_APP,
+    UNINSTALL_SANDBOX_APP,
+    CREATE_STREAM_INSTALLER,
+    DESTORY_STREAM_INSTALLER,
+};
+
+/* SAID: 401-28 */
+enum class BundleStatusCallbackInterfaceCode : uint32_t {
+    ON_BUNDLE_STATE_CHANGED,
+};
+
+/* SAID: 401-43-8 */
+enum class BundleStreamInstallerInterfaceCode : uint32_t {
+    CREATE_STREAM = 0,
+    STREAM_INSTALL = 1,
+    CREATE_SHARED_BUNDLE_STREAM = 2,
+    CREATE_SIGNATURE_FILE_STREAM = 3
+};
+
+/* SAID: 401-26 */
+enum class CleanCacheCallbackInterfaceCode : uint32_t {
+    ON_CLEAN_CACHE_CALLBACK,
+};
+
+/* SAID: 401-43 */
+enum class StatusReceiverInterfaceCode : uint32_t {
+    ON_STATUS_NOTIFY,
+    ON_FINISHED,
+};
+
+/* SAID: 401-74 */
+enum class DefaultAppInterfaceCode : uint32_t {
+    IS_DEFAULT_APPLICATION = 0,
+    GET_DEFAULT_APPLICATION = 1,
+    SET_DEFAULT_APPLICATION = 2,
+    RESET_DEFAULT_APPLICATION = 3,
+};
+
+/* SAID: 401-100 */
+enum class OverlayManagerInterfaceCode : uint32_t {
+    GET_ALL_OVERLAY_MODULE_INFO = 0,
+    GET_OVERLAY_MODULE_INFO_BY_NAME = 1,
+    GET_OVERLAY_MODULE_INFO = 2,
+    GET_TARGET_OVERLAY_MODULE_INFOS = 3,
+    GET_OVERLAY_MODULE_INFO_BY_BUNDLE_NAME = 4,
+    GET_OVERLAY_BUNDLE_INFO_FOR_TARGET = 5,
+    GET_OVERLAY_MODULE_INFO_FOR_TARGET = 6,
+    SET_OVERLAY_ENABLED = 7,
+    SET_OVERLAY_ENABLED_FOR_SELF = 8,
+};
+
+/* SAID: 401-81 */
+enum class QuickFixManagerInterfaceCode : uint32_t {
+    DEPLOY_QUICK_FIX = 0,
+    SWITCH_QUICK_FIX = 1,
+    DELETE_QUICK_FIX = 2,
+    CREATE_FD = 3
+};
+
+/* SAID: 401-81 */
+enum class QuickFixStatusCallbackInterfaceCode : uint32_t {
+    ON_PATCH_DEPLOYED = 1,
+    ON_PATCH_SWITCHED = 2,
+    ON_PATCH_DELETED = 3
+};
+
+/* SAID: 401-53 */
 enum class BundleUserMgrInterfaceCode : uint32_t {
     CREATE_USER = 0,
     REMOVE_USER = 1,
