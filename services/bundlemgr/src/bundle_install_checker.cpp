13f721479ee4759f4d8deb47f9aae33fcfc9b243
Lotol <linxiangzhi@huawei.com>
2023-06-28 10:10:18 +0000

    update services/bundlemgr/src/bundle_install_checker.cpp.
    Signed-off-by: Lotol <linxiangzhi@huawei.com>

--- services/bundlemgr/src/bundle_install_checker.cpp
+++ services/bundlemgr/src/bundle_install_checker.cpp
@@ -280,7 +280,7 @@ ErrCode BundleInstallChecker::ParseHapFiles(
         newInfo.SetAppType(checkParam.appType);
         Security::Verify::ProvisionInfo provisionInfo = hapVerifyRes[i].GetProvisionInfo();
         bool isSystemApp = (provisionInfo.bundleInfo.appFeature == Constants::HOS_SYSTEM_APP ||
-            provisionInfo.bundleInfo.appFeature == Constants::OHOS_SYSTEM_APP) ||
+            provisionInfo.bundleInfo.appFeature == Constants::HOS_SYSTEM_APP) ||
             (bundlePaths[i].find(Constants::SYSTEM_APP_SCAN_PATH) == 0);
         if (isSystemApp) {
             newInfo.SetAppType(Constants::AppType::SYSTEM_APP);
