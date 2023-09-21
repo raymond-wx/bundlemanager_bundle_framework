7903e1e1baa741a256b016cd56b01706d7812811
Handsome_Afraican_Man <zhouwenzhi@huawei.com>
2023-09-21 20:56:25 +0800

    IssueNo:#I83DZI     Description:Bugfix     Sig:bundleManager     Feature or Bugfix:Bugfix     Binary Source:No
        Signed-off-by: zhouwenzhi <zhouwenzhi@huawei.com>

--- interfaces/kits/js/bundlemgr/native_module.cpp
+++ interfaces/kits/js/bundlemgr/native_module.cpp
@@ -60,8 +60,6 @@ static NativeValue* JsBundleMgrInit(NativeEngine* engine, NativeValue* exports)
     object->SetProperty("BundleFlag", reinterpret_cast<NativeValue*>(CreateBundleFlagObject(env)));
 
     const char *moduleName = "JsBundleMgr";
-    BindNativeFunction(*engine, *object, "getAllApplicationInfo", moduleName, JsBundleMgr::GetAllApplicationInfo);
-    BindNativeFunction(*engine, *object, "getApplicationInfo", moduleName, JsBundleMgr::GetApplicationInfo);
     BindNativeFunction(*engine, *object, "setAbilityEnabled", moduleName, JsBundleMgr::SetAbilityEnabled);
     BindNativeFunction(*engine, *object, "setApplicationEnabled", moduleName, JsBundleMgr::SetApplicationEnabled);
     BindNativeFunction(*engine, *object, "getAllBundleInfo", moduleName, JsBundleMgr::GetAllBundleInfo);
@@ -86,6 +84,8 @@ static napi_value Init(napi_env env, napi_value exports)
         DECLARE_NAPI_FUNCTION("queryAbilityByWant", QueryAbilityInfos),
         DECLARE_NAPI_FUNCTION("getBundleArchiveInfo", GetBundleArchiveInfo),
         DECLARE_NAPI_FUNCTION("getAbilityIcon", GetAbilityIcon),
+        DECLARE_NAPI_FUNCTION("getAllApplicationInfo", GetAllApplicationInfo),
+        DECLARE_NAPI_FUNCTION("getApplicationInfo", GetApplicationInfo),
     };
     NAPI_CALL(env, napi_define_properties(env, exports, sizeof(desc) / sizeof(desc[0]), desc));
 
