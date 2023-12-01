26ff1d9b5da12e16c5644814404f078cd637c3cc
Zhou Shihui <zhoushihui4@huawei.com>
2023-12-01 17:26:03 +0800

    支持以'.so.x'结尾的so解析
    Signed-off-by: Zhou Shihui <zhoushihui4@huawei.com>

--- services/bundlemgr/src/installd/installd_operator.cpp
+++ services/bundlemgr/src/installd/installd_operator.cpp
@@ -29,6 +29,7 @@
 #include <fcntl.h>
 #include <fstream>
 #include <map>
+#include <regex>
 #include <sstream>
 #include <string.h>
 #include <sys/mman.h>
@@ -50,6 +51,7 @@ static const char LIB64_DIFF_PATCH_SHARED_SO_PATH[] = "system/lib64/libdiff_patc
 static const char APPLY_PATCH_FUNCTION_NAME[] = "ApplyPatch";
 static std::string PREFIX_RESOURCE_PATH = "/resources/rawfile/";
 static std::string PREFIX_TARGET_PATH = "/print_service/";
+static const std::string SO_SUFFIX_REGEX = "\\.so\\.[0-9][0-9]*$";
 #if defined(CODE_SIGNATURE_ENABLE)
 using namespace OHOS::Security::CodeSign;
 #endif
@@ -80,7 +82,14 @@ static bool EndsWith(const std::string &sourceString, const std::string &targetS
     if (sourceString.length() < targetSuffix.length()) {
         return false;
     }
-    return sourceString.rfind(targetSuffix) == (sourceString.length() - targetSuffix.length());
+    if (sourceString.rfind(targetSuffix) == (sourceString.length() - targetSuffix.length())) {
+        return true;
+    }
+    if (targetSuffix == Constants::SO_SUFFIX) {
+        std::regex soRegex(SO_SUFFIX_REGEX);
+        return std::regex_search(sourceString, soRegex);
+    }
+    return false;
 }
 } // namespace
 
