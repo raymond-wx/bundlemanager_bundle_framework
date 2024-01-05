9a3758fe15cb8662771f926a1d314c1bee83e0c3
junyi233 <zhengjun29@huawei.com>
2024-01-05 10:11:44 +0800

    告警修复
    Signed-off-by: junyi233 <zhengjun29@huawei.com>

--- interfaces/inner_api/appexecfwk_base/include/form_info.h
+++ interfaces/inner_api/appexecfwk_base/include/form_info.h
@@ -40,7 +40,7 @@ struct FormInfo : public Parcelable {
     std::string scheduledUpdateTime = "";
     std::string src;
     FormWindow window;
-    int32_t displayNameId;
+    int32_t displayNameId = 0;
     int32_t descriptionId = 0;
     int32_t updateDuration = 0;
     int32_t defaultDimension = 0;
