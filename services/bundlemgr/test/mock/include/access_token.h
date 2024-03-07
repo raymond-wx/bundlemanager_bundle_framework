d2b3bd139987fff5ff9278453f27862c85a4a5e0
wangtiantian <wangtiantian19@huawei.com>
2024-03-07 09:13:14 +0800

    IssueNo:#I94Q1X Description:fix testcase bug Sig:SIG_ApplicaitonFramework Feature or Bugfix:Bugfix Binary Source:No Signed-off-by: wangtiantian <wangtiantian19@huawei.com>

--- services/bundlemgr/test/mock/include/access_token.h
+++ services/bundlemgr/test/mock/include/access_token.h
@@ -148,7 +148,7 @@ public:
     int32_t apiVersion;
     /** indicates whether the hap is a system app */
     bool isSystemApp;
-    /* app type 类型有哪些 */
+    /* app type */
     std::string appDistributionType;
 };
 
