b22d464cb73a3a5d9e25e67ffe00f71c7f1c7f15
Lotol <linxiangzhi@huawei.com>
2024-07-11 08:58:58 +0000

    update services/bundlemgr/src/bundle_mgr_host_impl.cpp.
    Signed-off-by: Lotol <linxiangzhi@huawei.com>

--- services/bundlemgr/src/bundle_mgr_host_impl.cpp
+++ services/bundlemgr/src/bundle_mgr_host_impl.cpp
@@ -2720,7 +2720,7 @@ bool BundleMgrHostImpl::VerifyCallingPermission(const std::string &permission)
 bool BundleMgrHostImpl::QueryExtensionAbilityInfoByUri(const std::string &uri, int32_t userId,
     ExtensionAbilityInfo &extensionAbilityInfo)
 {
-    LOG_D(BMS_TAG_QUERY, "uri : %{private}s, userId : %{public}d", uri.c_str(), userId);
+    LOG_I(BMS_TAG_QUERY, "uri : %{private}s, userId : %{public}d", uri.c_str(), userId);
     // API9 need to be system app, otherwise return empty data
     if (!BundlePermissionMgr::IsSystemApp() &&
         !BundlePermissionMgr::VerifyCallingBundleSdkVersion(ServiceConstants::API_VERSION_NINE)) {
