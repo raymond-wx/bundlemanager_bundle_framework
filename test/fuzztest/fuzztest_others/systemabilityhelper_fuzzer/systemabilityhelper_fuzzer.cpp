1b4e91e66bcfcc904436d1f34b6ee2e3b75fd9ef
sunjiakun <sunjiakun5@huawei.com>
2024-07-21 12:59:07 +0800

    包管理fuzz测试接口覆盖
    Signed-off-by: sunjiakun <sunjiakun5@huawei.com>

---
+++ test/fuzztest/fuzztest_others/systemabilityhelper_fuzzer/systemabilityhelper_fuzzer.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "systemabilityhelper_fuzzer.h"
+
+#include <iostream>
+
+#include "securec.h"
+#include "system_ability_helper.h"
+
+using namespace OHOS::AppExecFwk;
+namespace OHOS {
+constexpr size_t FOO_MAX_LEN = 1024;
+constexpr size_t U32_AT_SIZE = 4;
+constexpr uint8_t ENABLE = 2;
+bool DoSomethingInterestingWithMyAPI(const char* data, size_t size)
+{
+    SystemAbilityHelper helper;
+    int32_t systemAbilityId = reinterpret_cast<uintptr_t>(data);
+    helper.GetSystemAbility(systemAbilityId);
+    helper.RemoveSystemAbility(systemAbilityId);
+    sptr<IRemoteObject> systemAbility;
+    helper.AddSystemAbility(systemAbilityId, systemAbility);
+    std::string bundleName(data, size);
+    int32_t uid = reinterpret_cast<uintptr_t>(data);
+    helper.UninstallApp(bundleName, uid, systemAbilityId);
+    helper.UpgradeApp(bundleName, uid, systemAbilityId);
+    helper.UnloadSystemAbility(systemAbilityId);
+    return true;
+}
+} // namespace OHOS
+
+// Fuzzer entry point.
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    /* Run your code on data */
+    if (data == nullptr) {
+        return 0;
+    }
+
+    /* Validate the length of size */
+    if (size < OHOS::U32_AT_SIZE || size > OHOS::FOO_MAX_LEN) {
+        return 0;
+    }
+
+    char* ch = (char*)malloc(size + 1);
+    if (ch == nullptr) {
+        std::cout << "malloc failed." << std::endl;
+        return 0;
+    }
+
+    (void)memset_s(ch, size + 1, 0x00, size + 1);
+    if (memcpy_s(ch, size + 1, data, size) != EOK) {
+        std::cout << "copy failed." << std::endl;
+        free(ch);
+        ch = nullptr;
+        return 0;
+    }
+
+    OHOS::DoSomethingInterestingWithMyAPI(ch, size);
+    free(ch);
+    ch = nullptr;
+    return 0;
+}
