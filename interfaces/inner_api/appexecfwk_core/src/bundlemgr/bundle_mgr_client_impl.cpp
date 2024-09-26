857a5525e5f9256d4e4dbd65b3fd664db180ca38
ming-yue-liu1 <liumingyue12@huawei.com>
2024-09-26 20:36:07 +0800

    add getDir
    Signed-off-by: ming-yue-liu1 <liumingyue12@huawei.com>
    Change-Id: I64e4ef2bd610d229861f53a1be86fb1ed93182be

--- interfaces/inner_api/appexecfwk_core/src/bundlemgr/bundle_mgr_client_impl.cpp
+++ interfaces/inner_api/appexecfwk_core/src/bundlemgr/bundle_mgr_client_impl.cpp
@@ -530,6 +530,22 @@ ErrCode BundleMgrClientImpl::GetSandboxHapModuleInfo(const AbilityInfo &abilityI
     return bundleMgr_->GetSandboxHapModuleInfo(abilityInfo, appIndex, userId, hapModuleInfo);
 }
 
+ErrCode BundleMgrClientImpl::GetDirByBundleNameAndAppIndex(const std::string &bundleName, int32_t appIndex,
+        std::string &dataDir)
+{
+     APP_LOGD("GetDirByBundleNameAndAppIndex begin");
+    if (appIndex < 0) {
+        return ERR_BUNDLE_MANAGER_GET_DIR_INVALID_APP_INDEX;
+    }
+    if (appIndex == 0) {
+        dataDir = bundleName;
+    } else {
+        dataDir = "+clone-" + std::to_string(appIndex) + "+" + bundleName;
+
+    }
+    return ERR_OK;
+}
+
 ErrCode BundleMgrClientImpl::Connect()
 {
     APP_LOGD("connect begin");
