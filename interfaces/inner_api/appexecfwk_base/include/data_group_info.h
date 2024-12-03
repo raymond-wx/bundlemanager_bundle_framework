a4d0a05db64a7c238e67d3f19bfdfe64d6333b29
sunjiakun <sunjiakun5@huawei.com>
2024-12-03 11:00:16 +0800

    byte_align
    Signed-off-by: sunjiakun <sunjiakun5@huawei.com>

--- interfaces/inner_api/appexecfwk_base/include/data_group_info.h
+++ interfaces/inner_api/appexecfwk_base/include/data_group_info.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Copyright (c) 2023-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -25,11 +25,11 @@ namespace OHOS {
 namespace AppExecFwk {
 struct DataGroupInfo : public Parcelable {
 public:
-    std::string dataGroupId;
-    std::string uuid;
     int32_t uid = 0;
     int32_t gid = 0;
     int32_t userId = 0;
+    std::string dataGroupId;
+    std::string uuid;
 
     bool ReadFromParcel(Parcel &parcel);
     virtual bool Marshalling(Parcel &parcel) const override;
