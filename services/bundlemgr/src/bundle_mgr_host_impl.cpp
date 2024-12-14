6f87d5d44c281296851bedb103bbeab3d5ab3fa2
Zhou Shihui <zhoushihui4@huawei.com>
2024-12-14 15:31:30 +0800

    IPC接口安全排查问题
    Signed-off-by: Zhou Shihui <zhoushihui4@huawei.com>

--- services/bundlemgr/src/bundle_mgr_host_impl.cpp
+++ services/bundlemgr/src/bundle_mgr_host_impl.cpp
@@ -1505,6 +1505,12 @@ ErrCode BundleMgrHostImpl::CleanBundleCacheFiles(
         APP_LOGE("non-system app calling system api");
         return ERR_BUNDLE_MANAGER_SYSTEM_API_DENIED;
     }
+    if (!BundlePermissionMgr::VerifyCallingPermissionForAll(Constants::PERMISSION_REMOVECACHEFILE) &&
+        !BundlePermissionMgr::IsBundleSelfCalling(bundleName)) {
+        APP_LOGE("ohos.permission.REMOVE_CACHE_FILES permission denied");
+        EventReport::SendCleanCacheSysEventWithIndex(bundleName, userId, appIndex, true, true);
+        return ERR_BUNDLE_MANAGER_PERMISSION_DENIED;
+    }
     if (userId < 0) {
         APP_LOGE("userId is invalid");
         EventReport::SendCleanCacheSysEventWithIndex(bundleName, userId, appIndex, true, true);
@@ -1518,13 +1524,6 @@ ErrCode BundleMgrHostImpl::CleanBundleCacheFiles(
         APP_LOGE("the cleanCacheCallback is nullptr or bundleName empty");
         EventReport::SendCleanCacheSysEventWithIndex(bundleName, userId, appIndex, true, true);
         return ERR_BUNDLE_MANAGER_PARAM_ERROR;
-    }
-
-    if (!BundlePermissionMgr::VerifyCallingPermissionForAll(Constants::PERMISSION_REMOVECACHEFILE) &&
-        !BundlePermissionMgr::IsBundleSelfCalling(bundleName)) {
-        APP_LOGE("ohos.permission.REMOVE_CACHE_FILES permission denied");
-        EventReport::SendCleanCacheSysEventWithIndex(bundleName, userId, appIndex, true, true);
-        return ERR_BUNDLE_MANAGER_PERMISSION_DENIED;
     }
 
     if (isBrokerServiceExisted_ && !IsBundleExist(bundleName)) {
@@ -1671,17 +1670,17 @@ bool BundleMgrHostImpl::CleanBundleDataFiles(const std::string &bundleName, cons
         EventReport::SendCleanCacheSysEventWithIndex(bundleName, userId, appIndex, false, true);
         return false;
     }
+    if (!BundlePermissionMgr::VerifyCallingPermissionForAll(Constants::PERMISSION_REMOVECACHEFILE)) {
+        APP_LOGE("ohos.permission.REMOVE_CACHE_FILES permission denied");
+        EventReport::SendCleanCacheSysEventWithIndex(bundleName, userId, appIndex, false, true);
+        return false;
+    }
     if (bundleName.empty() || userId < 0) {
         APP_LOGE("the  bundleName empty or invalid userid");
         EventReport::SendCleanCacheSysEventWithIndex(bundleName, userId, appIndex, false, true);
         return false;
     }
     if (!CheckAppIndex(bundleName, userId, appIndex)) {
-        EventReport::SendCleanCacheSysEventWithIndex(bundleName, userId, appIndex, false, true);
-        return false;
-    }
-    if (!BundlePermissionMgr::VerifyCallingPermissionForAll(Constants::PERMISSION_REMOVECACHEFILE)) {
-        APP_LOGE("ohos.permission.REMOVE_CACHE_FILES permission denied");
         EventReport::SendCleanCacheSysEventWithIndex(bundleName, userId, appIndex, false, true);
         return false;
     }
@@ -1756,7 +1755,7 @@ bool BundleMgrHostImpl::RegisterBundleEventCallback(const sptr<IBundleEventCallb
         return false;
     }
     auto uid = IPCSkeleton::GetCallingUid();
-    if (uid != Constants::FOUNDATION_UID && uid != Constants::CODE_PROTECT_UID) {
+    if (uid != Constants::FOUNDATION_UID) {
         APP_LOGE("verify calling uid failed, uid : %{public}d", uid);
         return false;
     }
