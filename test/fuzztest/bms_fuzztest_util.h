89781bfe46fbe0ef3afb94334bc74f9eefd62b86
zhaogan <zhaogan2@huawei.com>
2025-05-07 14:39:35 +0800

    fuzz
    Signed-off-by: zhaogan <zhaogan2@huawei.com>

---
+++ test/fuzztest/bms_fuzztest_util.h
@@ -0,0 +1,158 @@
+/*
+* Copyright (c) 2025 Huawei Device Co., Ltd.
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+#ifndef BMS_FUZZTEST_UTIL_H
+#define BMS_FUZZTEST_UTIL_H
+
+#include <fuzzer/FuzzedDataProvider.h>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "application_info.h"
+
+namespace OHOS {
+namespace AppExecFwk {
+namespace BMSFuzzTestUtil {
+constexpr size_t STRING_MAX_LENGTH = 128;
+constexpr size_t ARRAY_MAX_LENGTH = 128;
+
+std::vector<std::string> GenerateStringArray(FuzzedDataProvider& fdp, size_t arraySizeMax = ARRAY_MAX_LENGTH,
+    size_t stringSize = STRING_MAX_LENGTH)
+{
+    std::vector<std::string> result;
+    size_t arraySize = fdp.ConsumeIntegralInRange<size_t>(0, arraySizeMax);
+    result.reserve(arraySize);
+
+    for (size_t i = 0; i < arraySize; ++i) {
+        std::string str = fdp.ConsumeRandomLengthString(stringSize);
+        result.emplace_back(str);
+    }
+
+    return result;
+}
+
+Resource GenerateResource(FuzzedDataProvider& fdp)
+{
+    Resource info;
+    info.id = fdp.ConsumeIntegral<uint32_t>();
+    info.bundleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    info.moduleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    return info;
+}
+
+void GenerateApplicationInfo(FuzzedDataProvider& fdp, ApplicationInfo &applicationInfo)
+{
+    applicationInfo.keepAlive = fdp.ConsumeBool();
+    applicationInfo.removable = fdp.ConsumeBool();
+    applicationInfo.singleton = fdp.ConsumeBool();
+    applicationInfo.userDataClearable = fdp.ConsumeBool();
+    applicationInfo.allowAppRunWhenDeviceFirstLocked = fdp.ConsumeBool();
+    applicationInfo.accessible = fdp.ConsumeBool();
+    applicationInfo.runningResourcesApply = fdp.ConsumeBool();
+    applicationInfo.associatedWakeUp = fdp.ConsumeBool();
+    applicationInfo.hideDesktopIcon = fdp.ConsumeBool();
+    applicationInfo.formVisibleNotify = fdp.ConsumeBool();
+    applicationInfo.isSystemApp = fdp.ConsumeBool();
+    applicationInfo.isLauncherApp = fdp.ConsumeBool();
+    applicationInfo.isFreeInstallApp = fdp.ConsumeBool();
+    applicationInfo.asanEnabled = fdp.ConsumeBool();
+    applicationInfo.debug = fdp.ConsumeBool();
+    applicationInfo.distributedNotificationEnabled = fdp.ConsumeBool();
+    applicationInfo.installedForAllUser = fdp.ConsumeBool();
+    applicationInfo.allowEnableNotification = fdp.ConsumeBool();
+    applicationInfo.allowMultiProcess = fdp.ConsumeBool();
+    applicationInfo.gwpAsanEnabled = fdp.ConsumeBool();
+    applicationInfo.enabled = fdp.ConsumeBool();
+    applicationInfo.hasPlugin = fdp.ConsumeBool();
+    applicationInfo.multiProjects = fdp.ConsumeBool();
+    applicationInfo.isCompressNativeLibs = fdp.ConsumeBool();
+    applicationInfo.tsanEnabled = fdp.ConsumeBool();
+    applicationInfo.hwasanEnabled = fdp.ConsumeBool();
+    applicationInfo.ubsanEnabled = fdp.ConsumeBool();
+    applicationInfo.cloudFileSyncEnabled = fdp.ConsumeBool();
+    applicationInfo.needAppDetail = fdp.ConsumeBool();
+    applicationInfo.versionCode = fdp.ConsumeIntegral<uint32_t>();
+    applicationInfo.apiCompatibleVersion = fdp.ConsumeIntegral<uint32_t>();
+    applicationInfo.iconId = fdp.ConsumeIntegral<uint32_t>();
+    applicationInfo.labelId = fdp.ConsumeIntegral<uint32_t>();
+    applicationInfo.descriptionId = fdp.ConsumeIntegral<uint32_t>();
+    applicationInfo.accessTokenId = fdp.ConsumeIntegral<uint32_t>();
+    applicationInfo.applicationReservedFlag = fdp.ConsumeIntegral<uint32_t>();
+    applicationInfo.apiTargetVersion = fdp.ConsumeIntegral<int32_t>();
+    applicationInfo.minCompatibleVersionCode = fdp.ConsumeIntegral<int32_t>();
+    applicationInfo.supportedModes = fdp.ConsumeIntegral<int32_t>();
+    applicationInfo.appIndex = fdp.ConsumeIntegral<int32_t>();
+    applicationInfo.uid = fdp.ConsumeIntegral<int32_t>();
+    applicationInfo.flags = fdp.ConsumeIntegral<int32_t>();
+    applicationInfo.targetPriority = fdp.ConsumeIntegral<int32_t>();
+    applicationInfo.overlayState = fdp.ConsumeIntegral<int32_t>();
+    applicationInfo.maxChildProcess = fdp.ConsumeIntegral<int32_t>();
+    applicationInfo.applicationFlags = fdp.ConsumeIntegral<int32_t>();
+    applicationInfo.bundleType = static_cast<BundleType>(fdp.ConsumeIntegralInRange<int32_t>(0, 4));
+    applicationInfo.crowdtestDeadline = fdp.ConsumeIntegral<int64_t>();
+    applicationInfo.accessTokenIdEx = fdp.ConsumeIntegral<uint64_t>();
+    applicationInfo.name = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.bundleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.versionName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.iconPath = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.label = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.description = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.asanLogPath = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.codePath = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.dataDir = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.dataBaseDir = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.cacheDir = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.entryDir = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.apiReleaseType = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.deviceId = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.entityType = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.process = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.vendor = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.appPrivilegeLevel = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.appDistributionType = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.appProvisionType = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.nativeLibraryPath = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.cpuAbi = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.arkNativeFilePath = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.arkNativeFileAbi = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.fingerprint = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.icon = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.entryModuleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.signatureKey = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.targetBundleName = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.compileSdkVersion = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.compileSdkType = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.organization = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.appDetailAbilityLibraryPath = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.installSource = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.configuration = fdp.ConsumeRandomLengthString(STRING_MAX_LENGTH);
+    applicationInfo.iconResource = GenerateResource(fdp);
+    applicationInfo.labelResource = GenerateResource(fdp);
+    applicationInfo.descriptionResource = GenerateResource(fdp);
+
+    applicationInfo.allowCommonEvent = GenerateStringArray(fdp);
+    applicationInfo.assetAccessGroups = GenerateStringArray(fdp);
+
+    // assign when calling the get interface
+    applicationInfo.permissions = GenerateStringArray(fdp);
+    applicationInfo.moduleSourceDirs = GenerateStringArray(fdp);
+    // Installation-free
+    applicationInfo.targetBundleList = GenerateStringArray(fdp);
+}
+}  // namespace BMSFuzzTestUtil
+}  // namespace AppExecFwk
+}  // namespace OHOS
+#endif  // BMS_FUZZTEST_UTIL_H
