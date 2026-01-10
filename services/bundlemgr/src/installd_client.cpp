cf7c3fe61ff8e2c592a6aa91d6322bbc7d1f30fa
liuyaoqian <liuyaoqian@h-partners.com>
2026-01-10 13:55:47 +0800

    update: 更新文件 installd_client.cpp
    Signed-off-by: liuyaoqian <liuyaoqian@h-partners.com>

--- services/bundlemgr/src/installd_client.cpp
+++ services/bundlemgr/src/installd_client.cpp
@@ -24,7 +24,7 @@
 namespace OHOS {
 namespace AppExecFwk {
 namespace {
-#ifdef VERIFY_PLAT_FPGA  //Used only for chip platform, do not modift.
+#ifdef VERIFY_PLAT_FPGA  //Used only for chip platform, do not modify.
 constexpr int32_t LOAD_SA_TIMEOUT_MS = 80 * 1000;
 #else
 constexpr int16_t LOAD_SA_TIMEOUT_MS = 4 * 1000;
